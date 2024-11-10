a5ac3a8c9b80365aed65db4692d3f1e241fca5a7
Александр Смирнов <sas2job@gmail.com>
2024-11-10 21:25:50 +0300

    Add users#crud

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update destroy]
+  # before_action :authenticate_user!, except: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'Пользователь успешно создан.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Пользователь успешно обновлен.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path, notice: 'Пользователь удален.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :role)
+  end
+end
