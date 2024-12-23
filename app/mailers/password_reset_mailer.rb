17f1e40f3ddd41257bc4b2a64956833274237dfa
Александр Смирнов <sas2job@gmail.com>
2024-12-23 12:10:52 +0300

    Fix mailer

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -2,6 +2,6 @@ class PasswordResetMailer < ApplicationMailer
   def reset_email
     @user = params[:user]
 
-    mail to @user.email, subject: I18n.t('password_reset_mailer.reset_email.subject')
+    mail to: @user.email, subject: I18n.t('password_reset_mailer.reset_email.subject')
   end
 end
